caad6034a5dd3e5d93c6b3ce14e7d77b2c54a7fb
aleksa520 <pavlovic1216@fon.bg.ac.rs>
2018-03-09 11:02:35 +0100

    Dodati komentari

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -32,6 +32,9 @@ public class Biblioteka implements BibliotekaInterfejs{
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
+
+		// Ovo je okej komentar
+		
 		if(naslov == null)
 			throw new RuntimeException("Morate uneti naslov");
 		
